d86f8b54f6a36f3b134e9a3521cb7efae874cd29
AdamTheisen <atheisen@anl.gov>
2025-12-23 14:28:42 -0600

    ENH: Updating with comments

--- io/get_data.py
+++ io/get_data.py
@@ -1,6 +1,12 @@
+"""
+This function allows for a user to download data from the relevant datasets.
+Be sure to create a token.json file with your ARM username and token.
+
+"""
 import json
 import act
 
+# Set Date Range
 startdate = '2025-12-01'
 enddate = '2025-12-31'
 
@@ -10,14 +16,12 @@ with open('./token.json') as f:
 username = data['username']
 token = data['token']
 
-#Specify datastream and date range for KAZR data
-
 sdate = ''.join(startdate.split('-'))
 edate = ''.join(enddate.split('-'))
 
+# Download data from each datastream
 datastreams = ['sgpminimplC1.b1', 'sgpmplpolfsC1.b1', 'sgpceil10mC1.b1', 'sgpceilpolC1.b1',
                'sgpdlfptC1.b1', 'sgpdlfptE13.b1', 'sgprlC1.a0', 'sgphsrlC1.a1']
-
 for ds in datastreams:
     data_dir = './data/' + ds
     result = act.discovery.download_arm_data(username, token, ds, startdate, enddate, output=data_dir)
